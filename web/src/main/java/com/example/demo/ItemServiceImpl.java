6f94c1fa9b5d7772a65fb172546cd745f36cf79e
namickey <teethdai@gmail.com>
2022-12-16 09:15:59 +0900

    add

--- web/src/main/java/com/example/demo/ItemServiceImpl.java
+++ web/src/main/java/com/example/demo/ItemServiceImpl.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+
+    Logger looger = LoggerFactory.getLogger(ItemServiceImpl.class);
+
     @Autowired
     private ItemMapper itemMapper;
 
     @Override
     public String getItem(String id) {
+        looger.info("getItem called.");
         Item item = new Item();
         item.setId(id);
         Item ret = itemMapper.search(item);
--- web_log_1/src/main/java/com/example/demo/HelloController.java
+++ web_log_1/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Autowired
-    ItemService itemService;
+    Logger looger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-
-        String word = itemService.getItem(name);
-        return String.format("Hello %s!", name + " " + word);
+        looger.info("HelloController called.");
+        return String.format("Hello %s!", name);
     }
 
 }
